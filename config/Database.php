fd622b3b14f4a72379610f652bf217f7dec93249
JLRoening <jlroening@mail.fhsu.edu>
2022-03-23 19:08:41 -0500

    Database
    Implemented config and database

---
+++ config/Database.php
@@ -0,0 +1,31 @@
+<?php 
+  class Database {
+
+    private $hostname;
+    private $database;
+    private $username;
+    private $password;
+
+    private $conn;
+
+    // DB Connect
+    public function connect() {
+      $url = getenv('JAWSDB_URL');
+      $dbparts = parse_url($url);
+  
+      $hostname = $dbparts['host'];
+      $username = $dbparts['user'];
+      $password = $dbparts['pass'];
+      $database = ltrim($dbparts['path'],'/');
+
+      try {
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+      }
+      catch(PDOException $e)
+      {
+        echo "Connection failed: " . $e->getMessage();
+      }
+    }
+  }
